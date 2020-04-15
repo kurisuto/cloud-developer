1b552115ec304dd80ec388e55e6f805c107923ad
Sean Crist <kurisuto@panix.com>
2020-04-15 14:50:27 -0400

    Getting dev branch back in sync with master

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": process.env.UDACITY_DIALECT,
+    "aws_region": process.env.UDACITY_AWS_REGION,
+    "aws_profile": process.env.UDACITY_AWS_PROFILE,
+    "aws_media_bucket": process.env.UDACITY_AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
