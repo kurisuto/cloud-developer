2ebb0a03e0682593169e5f000f03287763e5952a
Sean Crist <kurisuto@panix.com>
2020-04-15 19:52:36 -0400

    Implemented authentication

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -7,7 +7,8 @@ export const config = {
     "dialect": process.env.UDACITY_DIALECT,
     "aws_region": process.env.UDACITY_AWS_REGION,
     "aws_profile": process.env.UDACITY_AWS_PROFILE,
-    "aws_media_bucket": process.env.UDACITY_AWS_MEDIA_BUCKET
+    "aws_media_bucket": process.env.UDACITY_AWS_MEDIA_BUCKET,
+    "jwt_secret": process.env.JWT_SECRET
   },
   "prod": {
     "username": "",
