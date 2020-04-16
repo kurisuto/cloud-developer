7869473f0965c16d4f5823b8ec87e9467bb6e984
Sean Crist <kurisuto@panix.com>
2020-04-15 20:00:10 -0400

    Adding verbiage to Hello World to confirm deployment to EB

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -27,7 +27,7 @@ import { V0MODELS } from './controllers/v0/model.index';
 
   // Root URI call
   app.get( "/", async ( req, res ) => {
-    res.send( "/api/v0/" );
+    res.send( "Hello, world!  This is /api/v0/" );
   } );
   
 
